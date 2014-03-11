b5b03c51cab4f2bb2f2910f35bc95e01a5d66be0
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2014-03-11 20:58:12 +0100

    Initial Commit

---
+++ SpotifyPuzzle_zipfsong/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SpotifyPuzzle_zipfsong
+//
+//  Created by Cedric Lamoriniere on 11/03/14.
+//  Copyright (c) 2014 Cedric Lamoriniere. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
